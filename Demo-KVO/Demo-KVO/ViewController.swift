a3b7361da3a7fd05c2abb28b1fd926aac7f06d72
Kristina Gelzinyte <kriglz@Kristinas-iMacs.local>
2018-05-02 12:41:00 +0300

    init kvo project

---
+++ Demo-KVO/Demo-KVO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-KVO
+//
+//  Created by Kristina Gelzinyte on 5/2/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
