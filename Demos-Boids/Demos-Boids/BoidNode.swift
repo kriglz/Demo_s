f1095bf8c898e66333701bcd98028d9ac5ddb1da
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-23 14:03:15 -0700

    init of boid node

---
+++ Demos-Boids/Demos-Boids/BoidNode.swift
@@ -0,0 +1,35 @@
+//
+//  BoidNode.swift
+//  Demos-Boids
+//
+//  Created by Kristina Gelzinyte on 8/23/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import SpriteKit
+
+class BoidNode: SKShapeNode {
+
+    private var boidPath: CGPath {
+        let triangleBezierPath = UIBezierPath()
+        triangleBezierPath.move(to: CGPoint.zero)
+        triangleBezierPath.addLine(to: CGPoint(x: 20, y: 0))
+        triangleBezierPath.addLine(to: CGPoint(x: 20, y: 0))
+        triangleBezierPath.addLine(to: CGPoint(x: 10, y: 30))
+        triangleBezierPath.close()
+        return triangleBezierPath.cgPath
+    }
+    
+    override init() {
+        super.init()
+        
+        self.path = boidPath
+        fillColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
--- Demos-Boids/Demos-Boids/GameScene.swift
+++ Demos-Boids/Demos-Boids/GameScene.swift
@@ -11,77 +11,30 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label = SKLabelNode(text: "HelloLabel")
-    private var spinnyNode = SKShapeNode()
+    // MARK: - Properties
     
+    private var label = SKLabelNode(text: "HelloLabel")
+    private var boidNode = BoidNode()
+    
+    // MARK: - Lifecycle functions
+
     override func didMove(to view: SKView) {
-        
         self.addChild(label)
-        self.addChild(spinnyNode)
 
         label.position = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height / 2)
         label.fontSize = 30
-        
-        // Get label node from scene and store it for use later
         label.alpha = 0.0
         label.run(SKAction.fadeIn(withDuration: 2.0))
         
-        // Create shape node to use during mouse interaction
-        let w = (view.frame.size.width + view.frame.size.height) * 0.05
-        spinnyNode = SKShapeNode(rectOf: CGSize(width: w, height: w), cornerRadius: w * 0.3)
-        
-        spinnyNode.lineWidth = 2.5
-        
-        spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-        spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                          SKAction.fadeOut(withDuration: 0.5),
-                                          SKAction.removeFromParent()]))
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode.copy() as? SKShapeNode {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        label.removeAllActions()
-        label.run(SKAction.fadeOut(withDuration: 1.0))
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        boidNode = BoidNode()
+        self.addChild(boidNode)
+        boidNode.position = CGPoint(x: 50, y: 50)
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    // MARK: - Node setup
+
 }
