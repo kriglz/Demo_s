8c4840cbdd8fdbdfe99742ab1fd15cfe6f9f673d
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-23 18:16:24 -0700

    Move action to boid node

--- Demos-Boids/Demos-Boids/BoidNode.swift
+++ Demos-Boids/Demos-Boids/BoidNode.swift
@@ -10,19 +10,37 @@ import SpriteKit
 
 class BoidNode: SKShapeNode {
 
+    // MARK: - Constants
+    
+    static let uniqueName = "Boid"
+    static let length = 30
+
+    private let timeCounterConstant = 20
+
+    // MARK: - Properties
+    
+    var neightbourBoidNodes = [BoidNode]()
+    
+    private(set) var direction = CGVector.zero
+    
+    private lazy var timeCounter = timeCounterConstant + 1
+    
     private var boidPath: CGPath {
         let triangleBezierPath = UIBezierPath()
         triangleBezierPath.move(to: CGPoint.zero)
         triangleBezierPath.addLine(to: CGPoint(x: 20, y: 0))
         triangleBezierPath.addLine(to: CGPoint(x: 20, y: 0))
-        triangleBezierPath.addLine(to: CGPoint(x: 10, y: 30))
+        triangleBezierPath.addLine(to: CGPoint(x: 10, y: BoidNode.length))
         triangleBezierPath.close()
         return triangleBezierPath.cgPath
     }
     
+    // MARK: - Initialization
+        
     override init() {
         super.init()
         
+        name = BoidNode.uniqueName
         path = boidPath
         fillColor = .white
     }
@@ -31,5 +49,28 @@ class BoidNode: SKShapeNode {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    // MARK: - Actions
+    
+    func move() {
+        timeCounter += 1
+        
+        if timeCounter > timeCounterConstant {
+            let x = CGFloat.random(min: -10, max: 10) / 10
+            let y = CGFloat.random(min: -10, max: 10) / 10
+            
+            direction = CGVector(dx: x, dy: y)
+            timeCounter = 0
+        }
+        
+        if !neightbourBoidNodes.isEmpty {
+            var neightbourBoidNodePositions = [direction]
+            for neighbour in neightbourBoidNodes {
+                neightbourBoidNodePositions.append(neighbour.direction)
+            }
+            
+            direction = neightbourBoidNodePositions.average
+        }
+        
+        position = position.applying(CGAffineTransform(translationX: direction.dx, y: direction.dy))
+    }
 }
--- Demos-Boids/Demos-Boids/GameScene.swift
+++ Demos-Boids/Demos-Boids/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
     private let boidNode = BoidNode()
     private let obstacleNode = ObstacleNode()
     
+    private var allBoids = [BoidNode]()
+    
     // MARK: - Lifecycle functions
 
     override func didMove(to view: SKView) {
@@ -23,22 +25,24 @@ class GameScene: SKScene {
         
         self.addChild(boidNode)
         boidNode.position = CGPoint(x: 50, y: 50)
+        allBoids.append(boidNode)
         
-        self.addChild(obstacleNode)
-        obstacleNode.position = CGPoint(x: 100, y: 100)
+//        self.addChild(obstacleNode)
+//        obstacleNode.position = CGPoint(x: 100, y: 100)
         
-        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
-        doubleTapGestureRecognizer.numberOfTapsRequired = 2
-        view.addGestureRecognizer(doubleTapGestureRecognizer)
+//        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
+//        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+//        view.addGestureRecognizer(doubleTapGestureRecognizer)
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         tapGestureRecognizer.numberOfTapsRequired = 1
-        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
+//        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
         view.addGestureRecognizer(tapGestureRecognizer)
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        evaluateNeighborBoids()
+        allBoids.forEach { $0.move() }
     }
     
     // MARK: - Event control
@@ -53,19 +57,35 @@ class GameScene: SKScene {
         newBoidNode.position = position
         newBoidNode.fillColor = SKColor.green
         self.addChild(newBoidNode)
+        allBoids.append(newBoidNode)
     }
     
-    @objc private func handleDoubleTap(_ gestureRecognizer: UITapGestureRecognizer) {
-        guard let view = self.view else { return }
-        
-        let flipTransform = CGAffineTransform(scaleX: 1, y: -1).concatenating(CGAffineTransform.init(translationX: 0, y: view.frame.height))
-        let position = gestureRecognizer.location(in: view).applying(flipTransform)
-
-        guard let newObstacleNode = obstacleNode.copy() as? ObstacleNode else { return }
-        newObstacleNode.position = position
-        self.addChild(newObstacleNode)
-    }
+//    @objc private func handleDoubleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+//        guard let view = self.view else { return }
+//
+//        let flipTransform = CGAffineTransform(scaleX: 1, y: -1).concatenating(CGAffineTransform.init(translationX: 0, y: view.frame.height))
+//        let position = gestureRecognizer.location(in: view).applying(flipTransform)
+//
+//        guard let newObstacleNode = obstacleNode.copy() as? ObstacleNode else { return }
+//        newObstacleNode.position = position
+//        self.addChild(newObstacleNode)
+//    }
     
-    // MARK: - Node setup
-
+    // MARK: - Node control
+    
+    private func evaluateNeighborBoids() {
+        for boid in allBoids {
+            let neighbourBoids = allBoids.filter { possiblyNeighbourBoid in
+                guard boid != possiblyNeighbourBoid else { return false }
+                
+                if boid.position.distance(to: possiblyNeighbourBoid.position) < CGFloat(BoidNode.length * 2) {
+                    return true
+                }
+                return false
+            }
+            
+            boid.neightbourBoidNodes = neighbourBoids
+        }
+    }
 }
+
