60b53368b2b69b2b6302b90d096ff3ab3e2467d5
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-24 19:16:24 -0700

    fixed random number generator

--- Demo-Boids/Demo-Boids/Utilities/CGFloat+Extension.swift
+++ Demo-Boids/Demo-Boids/Utilities/CGFloat+Extension.swift
@@ -12,6 +12,11 @@ extension CGFloat {
     
     /// Returns random number from the range.
     static func random(min: CGFloat, max: CGFloat) -> CGFloat {
-        return min + CGFloat(arc4random_uniform(UInt32(max - min + 1)))
+        return CGFloat.random * (max - min) + min
+    }
+    
+    /// Returns a random floating point number between 0.0 and 1.0.
+    public static var random: CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
 }
