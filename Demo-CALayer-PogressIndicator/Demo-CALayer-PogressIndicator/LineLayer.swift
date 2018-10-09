c01232eee291685f772df361238d841f659323d5
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-09 10:10:12 -0700

    Line layer implementation.

---
+++ Demo-CALayer-PogressIndicator/Demo-CALayer-PogressIndicator/LineLayer.swift
@@ -0,0 +1,90 @@
+//
+//  LineLayer.swift
+//  Demo-CALayer-PogressIndicator
+//
+//  Created by Kristina Gelzinyte on 10/9/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class LineLayer: CAShapeLayer {
+    
+    convenience init(path: CGPath) {
+        self.init()
+        self.path = path
+    }
+    
+    override init() {
+        super.init()
+        
+        self.lineWidth = 2
+        self.fillColor = UIColor.clear.cgColor
+        self.strokeColor = UIColor.blue.cgColor
+        
+        self.shadowColor = self.strokeColor
+        self.shadowOffset = .zero
+        self.shadowRadius = 4
+        self.shadowOpacity = 1
+        self.masksToBounds = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update(path: CGPath) {
+        self.path = path
+    }
+    
+    func strokeAnimation(duration: Double, delay: Double, repeatCount: Float) {
+        let startAnimation = CABasicAnimation(keyPath: "strokeStart")
+        startAnimation.fromValue = 1
+        startAnimation.toValue = 0
+        startAnimation.duration = duration
+        startAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        startAnimation.isRemovedOnCompletion = false
+        startAnimation.fillMode = CAMediaTimingFillMode.forwards
+        
+        let endAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        endAnimation.fromValue = 1
+        endAnimation.toValue = 0
+        endAnimation.duration = duration
+        endAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        endAnimation.beginTime = startAnimation.beginTime + delay
+        
+        let strokeAnimation = CAAnimationGroup()
+        strokeAnimation.animations = [startAnimation, endAnimation]
+        strokeAnimation.duration = duration + delay
+        strokeAnimation.repeatCount = repeatCount
+        self.add(strokeAnimation, forKey: "strokeAnimation")
+    }
+    
+    func rotationAnimation(duration: Double, repeatCount: Float){
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotationAnimation.byValue = -CGFloat.pi / 3
+        rotationAnimation.duration = duration / 6
+        rotationAnimation.isCumulative = true
+        rotationAnimation.repeatCount = repeatCount
+        
+        self.add(rotationAnimation, forKey: rotationAnimation.keyPath)
+    }
+    
+    func colorAnimation(duration: Double, repeatCount: Float) {
+        let colorAnimation = CABasicAnimation(keyPath: "strokeColor")
+        colorAnimation.toValue = UIColor.red.cgColor
+        colorAnimation.duration = duration / 2
+        colorAnimation.autoreverses = true
+        
+        let shadowColorAnimation = CABasicAnimation(keyPath: "shadowColor")
+        shadowColorAnimation.toValue = UIColor.red.cgColor
+        shadowColorAnimation.duration = duration / 2
+        shadowColorAnimation.autoreverses = true
+        
+        let strokeColorAnimation = CAAnimationGroup()
+        strokeColorAnimation.animations = [colorAnimation, shadowColorAnimation]
+        strokeColorAnimation.duration = duration
+        strokeColorAnimation.repeatCount = repeatCount
+        self.add(strokeColorAnimation, forKey: "strokeColorAnimation")
+    }
+}
--- Demo-CALayer-PogressIndicator/Demo-CALayer-PogressIndicator/ViewController.swift
+++ Demo-CALayer-PogressIndicator/Demo-CALayer-PogressIndicator/ViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -30,74 +25,16 @@ class ViewController: UIViewController {
                           width: width,
                           height: width)
         
-        let lineLayer = CAShapeLayer()
-        lineLayer.path = CGPath(ellipseIn: rect, transform: nil)
-        
-        lineLayer.lineWidth = 2
-        lineLayer.fillColor = UIColor.clear.cgColor
-        lineLayer.strokeColor = UIColor.blue.cgColor
-        
-        lineLayer.shadowColor = lineLayer.strokeColor
-        lineLayer.shadowOffset = .zero
-        lineLayer.shadowRadius = 4
-        lineLayer.shadowOpacity = 1
-        lineLayer.masksToBounds = false
-        
-        lineLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        lineLayer.anchorPointZ = 0.5
-        lineLayer.position = view.center
-        
+        let lineLayer = LineLayer(path: CGPath(ellipseIn: rect, transform: nil))
+        lineLayer.position = CGPoint(x: view.frame.midX, y: view.frame.midY)
         view.layer.addSublayer(lineLayer)
         
         let duration = 2.0
         let delay = 0.5
+        let repeatCount = Float.infinity
         
-        let startAnimation = CABasicAnimation(keyPath: "strokeStart")
-        startAnimation.fromValue = 1
-        startAnimation.toValue = 0
-        startAnimation.duration = duration
-        startAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        startAnimation.isRemovedOnCompletion = false
-        startAnimation.fillMode = CAMediaTimingFillMode.forwards
-        
-        let endAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        endAnimation.fromValue = 1
-        endAnimation.toValue = 0
-        endAnimation.duration = duration
-        endAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        endAnimation.beginTime = startAnimation.beginTime + delay
-
-        let strokeAnimation = CAAnimationGroup()
-        strokeAnimation.animations = [startAnimation, endAnimation]
-        strokeAnimation.duration = duration + delay
-        strokeAnimation.repeatCount = .infinity
-        lineLayer.add(strokeAnimation, forKey: "strokeAnimation")
-        
-        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        rotationAnimation.byValue = -CGFloat.pi / 3
-        rotationAnimation.duration = duration / 6
-        rotationAnimation.isCumulative = true
-        rotationAnimation.repeatCount = .infinity
-        
-        lineLayer.add(rotationAnimation, forKey: rotationAnimation.keyPath)
-        
-        let colorAnimation = CABasicAnimation(keyPath: "strokeColor")
-        colorAnimation.toValue = UIColor.red.cgColor
-        colorAnimation.duration = (duration + delay) / 2
-        colorAnimation.autoreverses = true
-
-        lineLayer.add(colorAnimation, forKey: colorAnimation.keyPath)
-        
-        let shadowColorAnimation = CABasicAnimation(keyPath: "shadowColor")
-        shadowColorAnimation.toValue = UIColor.red.cgColor
-        shadowColorAnimation.duration = (duration + delay) / 2
-        shadowColorAnimation.autoreverses = true
-        
-        let strokeColorAnimation = CAAnimationGroup()
-        strokeColorAnimation.animations = [colorAnimation, shadowColorAnimation]
-        strokeColorAnimation.duration = duration + delay
-        strokeColorAnimation.repeatCount = .infinity
-        lineLayer.add(strokeColorAnimation, forKey: "strokeColorAnimation")
+        lineLayer.strokeAnimation(duration: duration, delay: delay, repeatCount: repeatCount)
+        lineLayer.rotationAnimation(duration: duration, repeatCount: repeatCount)
+        lineLayer.colorAnimation(duration: duration + delay, repeatCount: repeatCount)
     }
 }
-
