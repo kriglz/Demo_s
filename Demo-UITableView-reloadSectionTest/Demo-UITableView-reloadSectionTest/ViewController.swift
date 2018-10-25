66b729f42375d2decdf219a387bfc8141b442275
Kristina Gelzinyte <kristina@kmacbookpro.local>
2018-10-25 17:39:04 +0300

    Implemented cell insertion and deletion

--- Demo-UITableView-reloadSectionTest/Demo-UITableView-reloadSectionTest/ViewController.swift
+++ Demo-UITableView-reloadSectionTest/Demo-UITableView-reloadSectionTest/ViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     let cellID = "MyCellId"
+    var dataSource = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.separatorColor = .clear
 
         tableView.dataSource = self
         tableView.delegate = self
@@ -36,24 +36,49 @@ class ViewController: UIViewController {
         self.tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         self.tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
+    
+    func delete(at index: Int) {
+        self.dataSource.remove(at: index)
+        
+        self.tableView.performBatchUpdates( {
+            self.tableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+        }, completion: nil)
+    }
+    
+    func duplicate(at index: Int) {
+        self.dataSource.insert(index + 1, at: index)
+
+        self.tableView.performBatchUpdates( {
+            self.tableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+        }, completion: nil)
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.dataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        
-        cell.textLabel?.text = "\(indexPath.item)"
-        cell.backgroundColor = UIColor(red: CGFloat.random(in: 0...1), green: 0, blue: CGFloat.random(in: 0...1), alpha: 1)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellID, for: indexPath)
+        cell.textLabel?.text = "\(self.dataSource[indexPath.item])"
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let duplicateAction = UITableViewRowAction(style: .normal, title: "Duplicate") { (action, indexPath) in
+            self.duplicate(at: indexPath.item)
+        }
+        
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            self.delete(at: indexPath.item)
+        }
+
+        return [deleteAction, duplicateAction]
     }
 }
