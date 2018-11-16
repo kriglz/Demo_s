17a0e36c9e64463196b6dec5f6efe81a90153655
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-11-16 20:24:49 +0200

    Init

---
+++ Demo-HitTest/Demo-HitTest/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Demo-HitTest
+//
+//  Created by Kristina Gelzinyte on 11/16/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
