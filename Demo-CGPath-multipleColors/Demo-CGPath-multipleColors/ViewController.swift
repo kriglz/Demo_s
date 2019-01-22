510a7b45978aa3cf33e884c1c6c379d4cb5f1212
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-01-22 21:33:22 +0200

    Custom layer

--- Demo-CGPath-multipleColors/Demo-CGPath-multipleColors/ViewController.swift
+++ Demo-CGPath-multipleColors/Demo-CGPath-multipleColors/ViewController.swift
@@ -25,11 +25,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let layer = self.colorLayer
-        layer.frame = self.view.bounds
+        let layer = ColorLayer()
+        let width = CGFloat(100.0)
+        layer.frame = CGRect(x: (self.view.frame.size.width - width) / 2,
+                             y: (self.view.frame.size.height - width) / 2,
+                             width: width,
+                             height: width)
         self.view.layer.addSublayer(layer)
+        
+        layer.setNeedsDisplay()
     }
 }
 
+class ColorLayer: CAShapeLayer {
+    
+    override func draw(in ctx: CGContext) {
+        super.draw(in: ctx)
+        
+        let path1 = UIBezierPath(ovalIn: self.bounds)
+        path1.lineWidth = 2
+        ctx.setStrokeColor(UIColor.red.cgColor)
+        ctx.setFillColor(UIColor.blue.cgColor)
+        ctx.addPath(path1.cgPath)
+        ctx.drawPath(using: .fillStroke)
+
+        let path2 = UIBezierPath(rect: self.bounds)
+        path2.lineWidth = 5
+        ctx.setStrokeColor(UIColor.green.cgColor)
+        ctx.addPath(path2.cgPath)
+        ctx.drawPath(using: .stroke)
+    }
+}
