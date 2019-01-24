2184b4dff29fbdeea2870b242027e8d1b265225d
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-01-24 21:12:30 +0200

    Second component

--- Demo-Clock.playground/Contents.swift
+++ Demo-Clock.playground/Contents.swift
@@ -9,21 +9,28 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode!
     
     override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
-        label = childNode(withName: "//LOVE") as? SKLabelNode
-        label.alpha = 0.0
-        let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 2.0),
-                                           .fadeOut(withDuration: 2.0)])
-        label.run(.repeatForever(fadeInOut))
+        label = SKLabelNode(text: "")
+        label.fontSize = 80
+        sceneView.scene?.addChild(label)
         
-        // Create shape node to use during mouse interaction
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
         let w = (size.width + size.height) * 0.3
+        spinnyNode = SKShapeNode(rect: CGRect(x: 0, y: 0, width: w * 0.01, height: w / 2))
+        spinnyNode.fillColor = UIColor.red
+        spinnyNode.lineWidth = 0
+        spinnyNode.strokeColor = .clear
         
-        spinnyNode = SKShapeNode(rectOf: CGSize(width: w, height: w), cornerRadius: w * 0.3)
-        spinnyNode.lineWidth = 2.5
-  
-        spinnyNode.run(.repeatForever(.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+        let seconds = Calendar.current.component(.second, from: Date())
+        spinnyNode.zRotation = -CGFloat.pi * CGFloat(seconds) / 30
+        spinnyNode.run(.repeatForever(.rotate(byAngle: -CGFloat.pi / 30, duration: 1)))
         sceneView.scene?.addChild(spinnyNode)
+    }
+    
+    @objc private func updateTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        label.text = formatter.string(from: Date())
     }
 }
 
