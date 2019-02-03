7742c09e2aeceada11a265c03cbf989aa6f905a1
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-02-03 10:35:48 +0200

    Sorting controller

--- Demo-sortingColors.playground/Sources/GraphView.swift
+++ Demo-sortingColors.playground/Sources/GraphView.swift
@@ -7,18 +7,12 @@ public class GraphView: UIView {
     public func performSorting() {
         let arraySize = 10
 
-        let unsortedArray = generateUnsortedArray(of: arraySize)
-        setupGraph(for: unsortedArray)
-
-        let sortingResult = InsertionSortingAlgorithm.sort(unsortedArray)
-        performSortingAnimation(sortingResult.sortingActions)
-    }
-        
-    func generateUnsortedArray(of size: Int) -> [Int] {
-        return [15, 09, 08, 01, 04, 11, 07, 12, 13, 06, 05, 03, 16, 02, 10, 14]
+        let sortingController = SortingController(element: arraySize)
+        setupGraph(for: sortingController.unsortedArray)
+        performSortingAnimation(sortingController.sortingActions)
     }
     
-    func setupGraph(for unsortedArray: [Int]) {
+    private func setupGraph(for unsortedArray: [Int]) {
         for (index, number) in unsortedArray.enumerated() {
             let rect = CGRect(x: Double(index) * width * 1.2 + 30.0,
                               y: 30.0,
@@ -34,8 +28,8 @@ public class GraphView: UIView {
     }
     
     func performSortingAnimation(_ actions: [SortingAction]) {
-        for action in actions {
-            swapElements(action.start, action.end, actionIndex: action.index)
+        actions.forEAch {
+            swapElements($0.start, $0.end, actionIndex: $0.index)
         }
     }
     
---
+++ Demo-sortingColors.playground/Sources/SortingController.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public class SortingController {
+    
+    private(set) var unsortedArray = [Int]()
+    private(set) var sortedArray = [Int]()
+    private(set) var sortionActions = [Int]()
+
+    public init(element count: Int) {
+        unsortedArray = generateUnsortedArray(of: count)
+        
+        let sortingResult = InsertionSortingAlgorithm.sort(unsortedArray)
+
+        sortedArray = sortingResult.sortedArray
+        sortionActions = sortingResult.sortingActions
+    }
+    
+    private func generateUnsortedArray(of size: Int) -> [Int] {
+        return [15, 09, 08, 01, 04, 11, 07, 12, 13, 06, 05, 03, 16, 02, 10, 14]
+    }
+    
+    
+    
+}
