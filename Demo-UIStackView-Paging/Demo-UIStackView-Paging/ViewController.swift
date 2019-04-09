6dc683d107ef7d16db496e319f9d11a7722271b4
kristina <kristina@planner5d.com>
2019-04-09 18:05:59 +0300

    content size improvement

--- Demo-UIStackView-Paging/Demo-UIStackView-Paging/ViewController.swift
+++ Demo-UIStackView-Paging/Demo-UIStackView-Paging/ViewController.swift
@@ -12,25 +12,41 @@ class ViewController: UIViewController {
 
     let contenView = HorizontallyScrollableStackView()
     let inset: CGFloat = 20
+    let itemWidth: CGFloat = 248
+    var spacing: CGFloat {
+        return 0.5 * self.inset
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.contenView.stackView.spacing = self.inset * 0.5
+        let controlLine = UIView()
+        controlLine.backgroundColor = .green
+        
+        self.contenView.stackView.spacing = self.spacing
         self.contenView.stackView.axis = .horizontal
         self.contenView.stackView.alignment = .leading
         self.contenView.stackView.distribution = .fillEqually
         
         self.contenView.scrollView.contentInset = UIEdgeInsets(top: 0, left: self.inset, bottom: 0, right: self.inset)
         
+        self.view.addSubview(controlLine)
         self.view.addSubview(self.contenView)
         
+        controlLine.translatesAutoresizingMaskIntoConstraints = false
         self.contenView.translatesAutoresizingMaskIntoConstraints = false
         
         self.contenView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         self.contenView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
 
         self.contenView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        controlLine.widthAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        controlLine.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        controlLine.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
+        controlLine.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: self.inset).isActive = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,7 +58,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.contenView.scrollView.contentSize = CGSize(width: self.contenView.scrollView.contentSize.width / 2, height: self.contenView.scrollView.contentSize.height)
+        let width = self.itemWidth * 3 + self.spacing + self.contenView.bounds.width - self.inset
+        let height = self.contenView.scrollView.contentSize.height
+        self.contenView.scrollView.contentSize = CGSize(width: width, height: height)
     }
 
     func reloadData() {
@@ -54,8 +72,8 @@ class ViewController: UIViewController {
             self.contenView.addArrangedSubview(item)
 
             item.translatesAutoresizingMaskIntoConstraints = false
-            item.heightAnchor.constraint(equalToConstant: 400).isActive = true
-            item.widthAnchor.constraint(equalTo: item.heightAnchor, multiplier: 0.62).isActive = true
+            item.widthAnchor.constraint(equalToConstant: self.itemWidth).isActive = true
+            item.heightAnchor.constraint(equalTo: item.widthAnchor, multiplier: 1.62).isActive = true
             
             if index == 3 {
                 self.contenView.stackView.setCustomSpacing(self.inset, after: item)
