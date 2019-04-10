e16567a87b9f3de5a9ec5cf5c9ff48bcf2da9d99
kristina <kristina@planner5d.com>
2019-04-10 10:10:39 +0300

    Paging implementation

--- Demo-UIStackView-Paging/Demo-UIStackView-Paging/ViewController.swift
+++ Demo-UIStackView-Paging/Demo-UIStackView-Paging/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
         let controlLine = UIView()
         controlLine.backgroundColor = .green
         
+        self.contenView.scrollView.delegate = self
+        self.contenView.scrollView.decelerationRate = .fast
+        
         self.contenView.stackView.spacing = self.spacing
         self.contenView.stackView.axis = .horizontal
         self.contenView.stackView.alignment = .leading
@@ -82,3 +85,72 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let proposedContentOffset = targetContentOffset.pointee
+        
+        // Translate those cell layoutAttributes into potential (candidate) scrollView offsets
+        let candidateOffsets: [CGFloat]? =  self.contenView.stackView.arrangedSubviews.map { view in
+            let contentInset = self.contenView.scrollView.contentInset
+            
+            if #available(iOS 11.0, *) {
+                return view.frame.origin.x - contentInset.left - self.contenView.scrollView.safeAreaInsets.left
+            } else {
+                return view.frame.origin.x - contentInset.left
+            }
+        }
+        
+        let bestCandidateOffset: CGFloat
+        
+        if velocity.x > 0 {
+            // If the scroll velocity was POSITIVE, then only consider cells/offsets to the RIGHT of the proposedContentOffset.x
+            let candidateOffsetsToRight = candidateOffsets?.toRight(ofProposedOffset: proposedContentOffset.x)
+            let nearestCandidateOffsetToRight = candidateOffsetsToRight?.nearest(toProposedOffset: proposedContentOffset.x)
+            bestCandidateOffset = nearestCandidateOffsetToRight ?? candidateOffsets?.last ?? proposedContentOffset.x
+        } else if velocity.x < 0 {
+            // If the scroll velocity was NEGATIVE, then only consider cells/offsets to the LEFT of the proposedContentOffset.x
+            let candidateOffsetsToLeft = candidateOffsets?.toLeft(ofProposedOffset: proposedContentOffset.x)
+            let nearestCandidateOffsetToLeft = candidateOffsetsToLeft?.nearest(toProposedOffset: proposedContentOffset.x)
+            bestCandidateOffset = nearestCandidateOffsetToLeft ?? candidateOffsets?.first ?? proposedContentOffset.x
+        } else {
+            // If the scroll velocity was ZERO we consider all `candidate` cells (regarless of whether they are to the left OR right of the proposedContentOffset.x)
+            // The cell/offset that is the NEAREST is the `bestCandidate`
+            let nearestCandidateOffset = candidateOffsets?.nearest(toProposedOffset: proposedContentOffset.x)
+            bestCandidateOffset = nearestCandidateOffset ?? proposedContentOffset.x
+        }
+        
+        targetContentOffset.pointee = CGPoint(x: bestCandidateOffset, y: proposedContentOffset.y)
+    }
+}
+
+fileprivate extension Sequence where Iterator.Element == CGFloat {
+    
+    func toLeft(ofProposedOffset proposedOffset: CGFloat) -> [CGFloat] {
+        return filter { candidateOffset in
+            return candidateOffset < proposedOffset
+        }
+    }
+    
+    func toRight(ofProposedOffset proposedOffset: CGFloat) -> [CGFloat] {
+        return filter { candidateOffset in
+            return candidateOffset > proposedOffset
+        }
+    }
+    
+    func nearest(toProposedOffset proposedOffset: CGFloat) -> CGFloat? {
+        guard let firstCandidateOffset = first(where: { _ in true }) else {
+            return nil
+        }
+        
+        return reduce(firstCandidateOffset) { (bestCandidateOffset: CGFloat, candidateOffset: CGFloat) -> CGFloat in
+            let candidateOffsetDistanceFromProposed = abs(candidateOffset - proposedOffset)
+            let bestCandidateOffsetDistancFromProposed = abs(bestCandidateOffset - proposedOffset)
+            
+            if candidateOffsetDistanceFromProposed < bestCandidateOffsetDistancFromProposed {
+                return candidateOffset
+            }
+            
+            return bestCandidateOffset
+        }
+    }
+}
