de38f775a29f471483bcbb6ea3593151cbe4f668
kristina <kristina@planner5d.com>
2019-04-19 10:17:19 +0300

    Solved masking issue

--- Demo-NSImageView-mask-tiling/Demo-NSImageView-mask-tiling/ViewController.swift
+++ Demo-NSImageView-mask-tiling/Demo-NSImageView-mask-tiling/ViewController.swift
@@ -11,27 +11,59 @@ import Cocoa
 class ViewController: NSViewController {
 
     let redView = NSView()
-    let mask = NSImageView()
+    let blueView = NSView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        blueView.wantsLayer = true
+        blueView.layer?.backgroundColor = NSColor.blue.cgColor
+        self.view.addSubview(blueView)
+        
         redView.wantsLayer = true
         redView.layer?.backgroundColor = NSColor.red.cgColor
         self.view.addSubview(redView)
-
-        mask.wantsLayer = true
-        mask.image = NSImage(named: "star.png")
     }
     
     override func viewDidAppear() {
         super.viewDidAppear()
 
+        blueView.frame = self.view.bounds
         redView.frame = self.view.bounds
+
+        setupStarMask()
+        setupCornerMask()
+    }
+    
+    func setupStarMask() {
+        let mask = CALayer()
+        mask.contents = NSImage(named: "star.png")
         
-        redView.layer?.mask = mask.layer
+        redView.layer?.mask = mask
         mask.frame = self.view.bounds
+    }
+    
+    func setupCornerMask() {
+        let mask = CALayer()
+
+        let image = NSImage(named: "Corners")?.slicedImage(targetRect: self.view.bounds)
+        mask.contents = image
         
-//        self.view.addSubview(mask)
+        blueView.layer?.mask = mask
+        mask.frame = self.view.bounds
+    }
+}
+
+extension NSImage {
+    func slicedImage(targetRect: NSRect) -> NSImage {
+        let rect = NSRect(origin: .zero, size: targetRect.size)
+        
+        let resultImage = NSImage.init(size: rect.size)
+        
+        resultImage.lockFocus()
+        self.draw(in: rect, from: NSRect(origin: .zero, size: self.size), operation: .copy, fraction: 1)
+        resultImage.unlockFocus()
+        
+        return resultImage
     }
 }
