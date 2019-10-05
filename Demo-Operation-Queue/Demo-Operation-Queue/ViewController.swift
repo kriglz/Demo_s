f1d00655ba4b4a6310e0093360f5a52b30a835af
kriglz <kristina.gelzinyte@gmail.com>
2019-10-04 17:12:21 -0700

    Operation queue

---
+++ Demo-Operation-Queue/Demo-Operation-Queue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-Operation-Queue
+//
+//  Created by Kristina Gelzinyte on 10/4/19.
+//  Copyright © 2019 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Demo-Operations/Demo-Operations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo-Operations
+//
+//  Created by Kristina Gelzinyte on 10/4/19.
+//  Copyright © 2019 Kristina Gelzinyte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
